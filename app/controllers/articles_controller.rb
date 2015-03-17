18b8b2b718c749c42f8ae79a896937131d9b2af1
Gosia Swider <gosia.mgs@gmail.com>
2015-03-17 13:26:33 +0000

    edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,10 @@ class ArticlesController < ApplicationController
   def new
   @article = Article.new
 end
- 
+ def edit
+  @article = Article.find(params[:id])
+end
+  
 def create
   @article = Article.new(article_params)
  
@@ -22,6 +25,16 @@ def create
     redirect_to @article
   else
     render 'new'
+  end
+end
+ 
+def update
+  @article = Article.find(params[:id])
+ 
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
   end
 end
  
