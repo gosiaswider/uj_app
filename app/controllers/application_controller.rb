e66e9333d1f48d4758a3a3b8ad4e2e7a7d96fd5f
Gosia Swider <gosia.mgs@gmail.com>
2015-03-24 14:31:31 +0000

    Acces

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user! 
+  load_and_authorize_resource
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,15 @@
 class ArticlesController < ApplicationController
- 
+  
+  #skip_before_action :authenticate_user! 
+load_and_authorize_resource
   def index
     @articles = Article.all
   end
   
   def show
-    @article = Article.find(params[:id])
-  end
+  @article = Article.find(params[:id])
+  authorize! :read, @article
+end
   
   def tresc
     @article = Article.find(params[:id])
