999c2ce4b6f605e43808928a27dbba733dd2241c
Gosia Swider <gosia.mgs@gmail.com>
2015-04-14 12:39:16 +0000

    add validation tests first and last name

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ group :development, :test do
   gem 'byebug'
 gem 'rspec'
   gem 'rspec-rails','~> 3.0'
+  
+  gem 'shoulda-matchers'
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe User do
+  it { should validate_presence_of :first_name }
+  
+  it { should validate_presence_of :last_name }
+  
   before :each do
     @user = User.new(first_name: 'John', last_name: 'Smith')
   end
