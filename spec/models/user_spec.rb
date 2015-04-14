492560e72a542194111427e052473c5c4e759b9a
Gosia Swider <gosia.mgs@gmail.com>
2015-04-14 12:30:52 +0000

    before each @user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 describe User do
+  before :each do
+    @user = User.new(first_name: 'John', last_name: 'Smith')
+  end
+  
   describe 'full name' do
     it 'returns user full name' do
-      user = User.new(first_name: 'John', last_name: 'Smith')
       
-      expect(user.full_name).to eq 'John Smith'
+      expect(@user.full_name).to eq 'John Smith'
     end
   end
-end
 
-describe User do
   describe 'initials' do
     it 'returns user initials' do
-      user = User.new(first_name: 'John', last_name: 'Smith')
-      
-      expect(user.initials).to eq 'JS'
+  
+      expect(@user.initials).to eq 'JS'
     end
   end
 end
