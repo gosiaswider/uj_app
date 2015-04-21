18c9fd26e37fe665f71de5ef443043675033f01b
Gosia Swider <gosia.mgs@gmail.com>
2015-04-21 13:24:12 +0000

    faeture click_link in articles_spec

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -11,7 +11,17 @@ feature 'articles' do
 
    expect(page).to have_content(@article.title)
    expect(page).to have_content(@second_article.title)
-     expect(page).to have_content('Listing Articles')
+   expect(page).to have_content('Listing Articles')
    end
+    
+    it 'user create a new article' do
+    visit articles_path
+
+    click_link('New article')
+
+    expect(page).to have_content('You are not authorized to access this page.')
+    expect(page).not_to have_content(@article.title)
+    expect(page.current_url).to eq 'http://www.example.com/'
+ end
  end
 end
