034a0af9ef44eb3771dea30fb4ede4194325e575
Gosia Swider <gosia.mgs@gmail.com>
2015-04-21 12:20:15 +0000

    add test show method of articles_controller

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,13 +1,26 @@
 require 'rails_helper'
 
 describe ArticlesController do
+  before :each do
+    @article = create(:article)
+  end
+  
   describe 'index' do
     it 'return articles' do
-      article = create(:article)
       get :index
       
       expect(response).to have_http_status(:success)
       expect(assigns(:articles).length).to eq 1
+    end
+  end
+  
+  describe 'show' do
+    it 'show an article' do
+      get :show, id: @article.id
+
+      expect(response).to have_http_status(:success)
+      expect(assigns(:article)).not_to eq nil
+      expect(assigns(:article).title).to eq @article.title
     end
   end
 end
