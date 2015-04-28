2d71184b2bf2ba1f4c498d7c07271668e7c2af19
Gosia Swider <gosia.mgs@gmail.com>
2015-04-28 13:05:33 +0000

    add validates pictures

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,7 @@ class Article < ActiveRecord::Base
   has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>"}
   validates :title, presence: true,
                     length: { minimum: 5 }
-  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/ 
+  validates_attachment_file_name :picture, :matches => [/gif\Z/, /png\Z/, /jpe?g\Z/] 
+  validates_with AttachmentSizeValidator, :attributes => :picture, :less_than => 1.megabytes
 end
